e768d1604001945086bc41a3a45d504476bdebfb
Jonathan Moallem <jdmoallem@gmail.com>
2020-02-02 16:07:05 +1100

    Added roll button and fixed buildings scene

--- scripts/building.gd
+++ scripts/building.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
+func _ready():
+	$Button.connect("button_down", self, "resolve_event")
+
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		if Input.is_action_pressed("lmb"):
 			get_parent().building_clicked(self)
+
+func resolve_event():
+	print("rolling...")
--- scripts/buildings.gd
+++ scripts/buildings.gd
@@ -2,13 +2,17 @@ extends Node2D
 
 onready var camera = $"../Camera2D"
 
+var zoomed = false
+
 func building_clicked(building):
-	print(building.name)
-	camera.zoom = Vector2(0.5, 0.5)
-	camera.position = building.global_position
+	if not zoomed:
+		camera.zoom = Vector2(0.5, 0.5)
+		camera.position = building.global_position
+		zoomed = true
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if Input.is_action_pressed("rmb"):
 			camera.zoom = Vector2(1, 1)
 			camera.position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2)
+			zoomed = false
