4c061b7a53d027c2b0cc4acc33159d4ca9bb5aad
Aryeh <aryehzinn@hotmail.com>
2020-02-02 12:32:35 +1100

    Added basic logic for changing turns

--- scripts/event_manager.gd
+++ scripts/event_manager.gd
@@ -3,8 +3,10 @@ extends Node2D
 # Turn timer
 onready var turn = 0
 
+# Resources enum for referencing buildings 
 enum Resources {FOOD, WATER, WALL, ARMS, INFLUENCE}
 
+# Events
 var events = {
 	'Food Rots!': {
 		
@@ -23,10 +25,46 @@ export (int) var walls
 export (int) var influence
 
 # Enemy Strength
-var enemies
+onready var enemies
 
 # Manpower
-var dice
+onready var dice = 20
+
+#Labels
+onready var turn_label = $TurnLabel
 
 func _ready():
+	turn_label.text = "Turn: " + str(turn)
+
+func process_turn():
+	
+	# 1. Resolve Combat
+	resolve_combat()
+	# 2. Enemy Turn
+	commit_enemies()
+	# 3. Degrade resources <--- walls degrade, food used, water used
+	degrade_resources()
+	# 4. Roll for Events 
+	generate_events()
+	# 5. Increment turn timer
+	increment_turn()
+
+func resolve_combat():
 	pass
+
+func commit_enemies():
+	pass
+
+func degrade_resources():
+	pass
+
+func generate_events():
+	pass
+
+func increment_turn():
+	turn += 1
+	turn_label.text = "Turn: " + str(turn)
+
+func _process(delta):
+	if Input.is_action_just_pressed('ui_accept'):
+		process_turn()
