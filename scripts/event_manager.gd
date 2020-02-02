609386820283e6f900dac6023c247054d00b456e
Jonathan Moallem <jdmoallem@gmail.com>
2020-02-02 11:27:16 +1100

    Added event list and resource enum

--- scripts/event_manager.gd
+++ scripts/event_manager.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
+enum Resources {FOOD, WATER, WALL, ARMS, INFLUENCE}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var events = {
+	'Food Rots!': {
+		
+	},
+	'Water Poisoned!': {},
+	'Riots Break Out!': {},
+	'Walls Start to Crumble!': {},
+	'Weapons Rust': {},
+}
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	print(Resources.INFLUENCE)
