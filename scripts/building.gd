5bc18484e7f6b584e33328dcf9c8d6e5bc95c53f
Jonathan Moallem <jdmoallem@gmail.com>
2020-02-03 13:22:58 +1100

    Added outline shader to buildings for highlighting

--- scripts/building.gd
+++ scripts/building.gd
@@ -2,11 +2,23 @@ extends Area2D
 
 func _ready():
 	$Button.connect("button_down", self, "resolve_event")
+	connect("mouse_entered", self, "highlight")
+	connect("mouse_exited", self, "highlight")
+
+func highlight():
+	if $Sprite.material:
+		var intensity = $Sprite.material.get_shader_param("intensity")
+		if intensity and intensity > 0:
+			$Sprite.material.set_shader_param("intensity", 0)
+		else:
+			$Sprite.material.set_shader_param("intensity", 200)
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		if Input.is_action_pressed("lmb"):
-			get_parent().building_clicked(self)
+	if event is InputEventMouseButton:
+		
+		if event.pressed:
+			if Input.is_action_pressed("lmb"):
+				get_parent().building_clicked(self)
 
 func resolve_event():
 	print("rolling...")
