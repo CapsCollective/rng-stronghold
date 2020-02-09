b8f005170caa428d77551511522bfa4ccbca63eb
Jonathan Moallem <jdmoallem@gmail.com>
2020-02-09 15:11:46 +1100

    Made dice-dragging work for dice

--- scenes/building_interface.gd
+++ scenes/building_interface.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 var physics_dice_scene = preload("res://scenes/physics_dice.tscn")
 var dice_to_roll = 0
+var target = 14
+var potential_dice
 
 func _ready():
 	$DiceButtons/RollDiceButton.connect("button_up", self, "roll_dice")
 	$DiceButtons/AddDiceButton.connect("button_up", self, "add_dice")
 	$DiceButtons/RemoveDiceButton.connect("button_up", self, "remove_dice")
+	update_display()
+
+func _process(delta):
+	for area in $Target/Area2D.get_overlapping_areas():
+		if not area.lifted:
+			target -= area.val
+			area.queue_free()
+			update_display()
 
 func add_dice():
 	dice_to_roll += 1
@@ -18,6 +28,7 @@ func remove_dice():
 
 func update_display():
 	$DiceButtons/DiceNumberLabel.text = "Dice Committed: " + str(dice_to_roll)
+	$Target/Label.text = "Target: " + str(target)
 
 func roll_dice():
 	for i in dice_to_roll:
