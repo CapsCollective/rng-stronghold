c1d46c842892ab1e4e495a9f4a8d9c5d202a3f63
Jonathan Moallem <jdmoallem@gmail.com>
2020-03-15 16:55:30 +1100

    Added negation labels to resource updates

--- scripts/resource_bar.gd
+++ scripts/resource_bar.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var game_manager = $"../../"
+var negation_label_scene = preload("res://scenes/negation_label.tscn")
 
 func update_labels():
 	$GrainLabel.text = "Grain: " + str(game_manager.grain)
@@ -11,3 +12,16 @@ func update_labels():
 	$TurnLabel.text = "Turn: " + str(game_manager.turn)
 	$EnemiesLabel.text = "Enemies: " + str(game_manager.enemies)
 	$DiceLabel.text = "Dice: " + str(game_manager.turn_dice) + "/" + str(game_manager.dice)
+	
+	display_label($GrainLabel, -5)
+	display_label($WaterLabel, -5)
+	display_label($InfluenceLabel, -5)
+	display_label($WeaponsLabel, -5)
+	display_label($WallsLabel, -5)
+	display_label($DiceLabel, -5)
+
+func display_label(resource_label, amount):
+	var negation_label = negation_label_scene.instance()
+	negation_label.set_text(str(amount))
+	negation_label.position = resource_label.get_global_transform().get_origin() + resource_label.get_size()/2
+	$"../".add_child(negation_label)
