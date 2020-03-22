c05c749b6bf29ce1640326a1364c32e7a3862636
Jonathan Moallem <jdmoallem@gmail.com>
2020-03-22 17:16:04 +1100

    Made all combat rolls occur simultaneously

--- scripts/combat_dice.gd
+++ scripts/combat_dice.gd
@@ -8,6 +8,7 @@ var finished_rotating = false
 var finished_moving = false
 var finished_resizing = false
 var forgiveness = 1
+var show = false
 
 func _ready():
 	if is_enemy_dice:
@@ -17,23 +18,25 @@ func _ready():
 	scale = Vector2(0.5, 0.5)
 
 func _process(delta):
-	if finished_rotating:
-		var new_position = position.linear_interpolate(final_position, delta*5)
-		var distance = abs(final_position.length() - new_position.length())
-		if distance < forgiveness and distance > -forgiveness:
-			if not finished_moving:
-				position = final_position
-				get_parent().get_parent().finished_rolling(self)
-				finished_moving = true
-		else:
-			position = new_position
-	else:
+	if not finished_rotating:
 		rotation = 0 + rotation / 1.5
 		if rotation < 0.01:
 			rotation = 0
 			finished_rotating = true
-	
-	if not finished_resizing:
+			get_parent().get_parent().finished_rolling(self)
+	elif not finished_moving and show:
+		var new_position = position.linear_interpolate(final_position, delta*5)
+		var distance = abs(final_position.length() - new_position.length())
+		if distance < forgiveness and distance > -forgiveness:
+				position = final_position
+				finished_moving = true
+				get_parent().get_parent().finished_showing(self)
+		else:
+			position = new_position
+	if not finished_resizing and show:
 		scale = scale.linear_interpolate(Vector2.ONE, delta*5)
 		if scale == Vector2.ONE:
 			finished_resizing = true
+
+func begin_show():
+	show = true
--- scripts/combat_interface.gd
+++ scripts/combat_interface.gd
@@ -1,36 +1,62 @@
 extends Node2D
 
 signal finished_rolling
+signal finished_showing
 
 onready var game_manager = $"../../"
 var physics_dice_scene = preload("res://scenes/physics_dice.tscn")
-var rolled = 0
 
-var expected_rolls = 2
-var dice_rolled = []
+var rolled = 0
+var expected_rolls = 0
+
+var shown = 0
+var expected_shown = 0
+
+
+
+var player_dice_rolled =[]
+var enemy_dice_rolled = []
 
 func start_resolve_combat():
 	visible = true
 	get_tree().get_root().set_disable_input(true)
 	MainCam.zoom_to($CombatZone.global_position, 0.75)
 	
-	while game_manager.enemies > 0:
-		rolled = 0
-		if game_manager.committed_dice > 0:
-			perform_dual_roll()
-			yield(self, "finished_rolling")
-		else:
-			break
-		game_manager.update_ui()
+	expected_rolls = game_manager.committed_dice + game_manager.enemies
 	
-	for enemy in range(game_manager.enemies):
-		perform_wall_roll()
-		yield(self, "finished_rolling")
-		if game_manager.walls <= 0:
-			print('GAME OVER')
-		game_manager.update_ui()
+	for i in range(game_manager.committed_dice):
+		var player_roll = game_manager.get_dice_roll()
+		player_dice_rolled.append(player_roll)
+		roll_dice($DiceSpawner1, player_roll, false)
+		yield(get_tree().create_timer(0.1), "timeout")
+	player_dice_rolled.clear()
 	
-	yield(get_tree().create_timer(1), "timeout")
+	for i in range(game_manager.enemies):
+		var enemy_roll = game_manager.get_dice_roll()
+		enemy_dice_rolled.append(enemy_roll)
+		roll_dice($DiceSpawner2, enemy_roll, true)
+		yield(get_tree().create_timer(0.1), "timeout")
+	enemy_dice_rolled.clear()
+	
+	yield(self, "finished_rolling")
+	
+	for i in range(enemy_dice_rolled.size()):
+		var player_dice = player_dice_rolled.pop_front()
+		var enemy_dice = enemy_dice_rolled.pop_front()
+		expected_shown = 1
+		if player_dice:
+			player_dice.begin_show()
+			expected_shown += 1
+		enemy_dice.begin_show()
+		yield(self, "finished_showing")
+		yield(get_tree().create_timer(0.5), "timeout")
+		display_results(player_dice.get("val"), enemy_dice.get("val"))
+		game_manager.update_ui()
+		player_dice.queue_free()
+		enemy_dice.queue_free()
+	
+	yield(get_tree().create_timer(2), "timeout")
+	reset_values()
 	MainCam.reset()
 	yield(get_tree().create_timer(2), "timeout")
 	
@@ -38,29 +64,34 @@ func start_resolve_combat():
 	visible = true
 	get_parent().get_parent().on_combat_resolved()
 
-func perform_dual_roll():
-	var player_roll = game_manager.get_dice_roll()
-	var enemy_roll = game_manager.get_dice_roll()
-	if player_roll > enemy_roll:
-		game_manager.enemies -= 1
-		game_manager.influence += 1
-	elif enemy_roll > player_roll:
-		game_manager.committed_dice -=1
-		game_manager.dice -= 1
-		game_manager.influence -= 1
-	else:
-		game_manager.enemies -= 1
-		game_manager.committed_dice -=1
+func reset_values():
+	for dice in (player_dice_rolled + enemy_dice_rolled):
+		dice.queue_free()
 	
-	expected_rolls = 2
-	roll_dice($DiceSpawner1, player_roll, false)
-	roll_dice($DiceSpawner2, enemy_roll, true)
+	player_dice_rolled.clear()
+	enemy_dice_rolled.clear()
+	
+	expected_rolls = 0
+	rolled = 0
+	expected_shown = 0
+	shown = 0
 
-func perform_wall_roll():
-	var enemy_roll = game_manager.get_dice_roll()
-	game_manager.walls -= enemy_roll
-	expected_rolls = 1
-	roll_dice($DiceSpawner2, enemy_roll, true)
+func display_results(player_roll, enemy_roll):
+	if player_roll:
+		if player_roll > enemy_roll:
+			game_manager.enemies -= 1
+			game_manager.influence += 1
+		elif enemy_roll > player_roll:
+			game_manager.committed_dice -=1
+			game_manager.dice -= 1
+			game_manager.influence -= 1
+		else:
+			game_manager.enemies -= 1
+			game_manager.committed_dice -=1
+	elif game_manager.walls > 0:
+		game_manager.walls -= enemy_roll
+	else:
+		print('GAME OVER')
 
 func roll_dice(spawner, value, enemy_dice):
 	var new_dice = physics_dice_scene.instance()
@@ -70,10 +101,15 @@ func roll_dice(spawner, value, enemy_dice):
 	spawner.add_child(new_dice)
 
 func finished_rolling(dice):
-	dice_rolled.append(dice)
+	if dice.is_enemy_dice:
+		enemy_dice_rolled.append(dice)
+	else:
+		player_dice_rolled.append(dice)
 	rolled += 1
-	if rolled % expected_rolls == 0:
-		for rolled_dice in dice_rolled:
-			rolled_dice.queue_free()
-		dice_rolled.clear()
+	if rolled >= expected_rolls:
 		emit_signal("finished_rolling")
+
+func finished_showing(dice):
+	shown += 1
+	if shown % expected_shown == 0:
+		emit_signal("finished_showing")
