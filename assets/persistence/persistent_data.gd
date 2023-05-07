9541de722ff252c55ca9d68e2c6f95002ffc6c7e
Jonathan Moallem <jdmoallem@icloud.com>
2023-05-08 00:37:20 +1000

    Added metadata functionality for persistent data

--- assets/persistence/persistent_data.gd
+++ assets/persistence/persistent_data.gd
@@ -8,7 +8,14 @@ enum DeserialisationResult {
 	FAILED = 2,
 }
 
-var save_sections : Dictionary = {}
+const PD_METADATA = "metadata"
+const PD_METADATA_SAVE_VERSION = "save_version"
+const PD_METADATA_SAVE_TIME = "save_time"
+
+const SAVE_VERSION: int = 0
+
+var save_metadata: Dictionary = {}
+var save_sections: Dictionary = {}
 
 func register_section(section: PersistentDataSection):
 	var tag = section.get_tag()
@@ -17,14 +24,27 @@ func register_section(section: PersistentDataSection):
 		return
 	save_sections[tag] = section
 
+func serialise_metadata() -> Dictionary:
+	return {
+		PD_METADATA_SAVE_VERSION: SAVE_VERSION,
+		PD_METADATA_SAVE_TIME: Time.get_datetime_string_from_system(true, true)
+	}
+
 func serialise_all_sections() -> Dictionary:
 	var save_data: Dictionary = {}
+	save_data[PD_METADATA] = serialise_metadata()
 	for section in save_sections.values():
 		save_data[section.get_tag()] = section.serialise()
 	return save_data
 
+func deserialise_metadata(data: Dictionary) -> DeserialisationResult:
+	if not data.has(PD_METADATA):
+		return DeserialisationResult.FAILED
+	save_metadata = data[PD_METADATA]
+	return DeserialisationResult.OK
+
 func deserialise_all_sections(data: Dictionary) -> DeserialisationResult:
-	var result: DeserialisationResult = DeserialisationResult.OK
+	var result: DeserialisationResult = deserialise_metadata(data)
 	for section in save_sections.values():
 		var tag = section.get_tag()
 		if not data.has(tag):
@@ -33,7 +53,7 @@ func deserialise_all_sections(data: Dictionary) -> DeserialisationResult:
 		if section_result == DeserialisationResult.FAILED:
 			push_error("Deserialisation Error: Failed to deserialise section \"", tag, "\".")
 		elif section_result == DeserialisationResult.RECOVERED:
-			push_error("Deserialisation Warning: Deserialisation of section \"", tag, "\" required recovery.")
+			push_warning("Deserialisation Warning: Deserialisation of section \"", tag, "\" required recovery.")
 		result = (section_result if section_result > result else result)
 	return result
 
@@ -55,4 +75,4 @@ func load_file():
 		var save_data = json.get_data()
 		if deserialise_all_sections(save_data) == DeserialisationResult.FAILED:
 			push_error("Deserialisation Error: Some sections failed to deserialise.")
-	print(serialise_all_sections())
+		Utils.push_log("Deserialised Data: ", save_data)
