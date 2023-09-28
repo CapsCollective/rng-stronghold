c6aa64269bab9d0f7423e1724909fbdb0f0d5f7d
Ben Kelly <benkellysoftware@gmail.com>
2023-09-28 20:24:57 +1000

    cleanup units panel code

--- assets/debug/scripts/debug_building.gd
+++ assets/debug/scripts/debug_building.gd
@@ -4,9 +4,5 @@ extends Control
 
 var rolls: Array
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func roll_assigned():
 	Utils.roll_dice(assigned_units.get_units())
--- assets/debug/scripts/debug_panel_units.gd
+++ assets/debug/scripts/debug_panel_units.gd
@@ -1,38 +1,27 @@
 extends DebugSection
 
-@onready var d4_total = $VBoxContainer/GridContainer/D4Total
-@onready var d4_available = $VBoxContainer/GridContainer/D4Available
-@onready var d6_total = $VBoxContainer/GridContainer/D6Total
-@onready var d6_available = $VBoxContainer/GridContainer/D6Available
-@onready var d8_total = $VBoxContainer/GridContainer/D8Total
-@onready var d8_available = $VBoxContainer/GridContainer/D8Available
+@onready var total_inputs = {
+	4: $VBoxContainer/GridContainer/D4Total,
+	6: $VBoxContainer/GridContainer/D6Total,
+	8: $VBoxContainer/GridContainer/D8Total
+}
 
+@onready var available_labels = {
+	4: $VBoxContainer/GridContainer/D4Available,
+	6: $VBoxContainer/GridContainer/D6Available,
+	8: $VBoxContainer/GridContainer/D8Available
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GameManager.units_changed.connect(on_units_changed)
-	d4_total.value_changed.connect(on_d4_total_changed)
-	d6_total.value_changed.connect(on_d6_total_changed)
-	d8_total.value_changed.connect(on_d8_total_changed)
+	for tier in total_inputs.keys():
+		total_inputs[tier].value_changed.connect(func(value): on_total_changed(tier, value))
 
-func on_d4_total_changed(value: int):
-	GameManager.set_total_units(4, value)
-	
-func on_d6_total_changed(value: int):
-	GameManager.set_total_units(6, value)
-
-func on_d8_total_changed(value: int):
-	GameManager.set_total_units(8, value)
+func on_total_changed(tier: int, value: int):
+	GameManager.set_total_units(tier, value)
 
 func on_units_changed(tier: int):
 	var units = Savegame.player.units[tier]
-	match tier:
-		4:
-			d4_total.set_value_no_signal(units.total)
-			d4_available.text = str(units.total - units.assigned)
-		6:
-			d6_total.set_value_no_signal(units.total)
-			d6_available.text = str(units.total - units.assigned)
-		8:
-			d8_total.set_value_no_signal(units.total)
-			d8_available.text = str(units.total - units.assigned)
+	total_inputs[tier].set_value_no_signal(units.total)
+	available_labels[tier].text = str(units.total - units.assigned)
