8a1b8429f114c6067ad73393d290dbd31df7ab28
Jonathan Moallem <jdmoallem@icloud.com>
2023-12-29 13:39:33 +1100

    Implemented JSON-based dialogue scripts

--- addons/dialogue/dialogue_plugin.gd
+++ addons/dialogue/dialogue_plugin.gd
@@ -1,24 +1,2 @@
 @tool
 extends EditorPlugin
-
-const DialogueScriptEditor = preload("res://addons/dialogue/scripts/dialogue_script_editor.gd")
-const DialogueScriptInspectorPlugin = preload("res://addons/dialogue/scripts/dialogue_script_inspector_plugin.gd")
-
-var ds_editor: DialogueScriptEditor
-var ds_inspector_plugin: DialogueScriptInspectorPlugin
-
-func _enter_tree():
-	ds_editor = DialogueScriptEditor.new()
-	add_control_to_bottom_panel(ds_editor, "Dialogue Editor")
-	ds_inspector_plugin = DialogueScriptInspectorPlugin.new()
-	ds_inspector_plugin.dialogue_script_inspected.connect(on_dialogue_script_inspected)
-	add_inspector_plugin(ds_inspector_plugin)
-
-func _exit_tree():
-	remove_inspector_plugin(ds_inspector_plugin)
-	remove_control_from_bottom_panel(ds_editor)
-	ds_editor.queue_free()
-
-func on_dialogue_script_inspected(dialogue_script: DialogueScript):
-	ds_editor.set_current_dialogue_script(dialogue_script)
-	make_bottom_panel_item_visible(ds_editor)
